131d47ce0a92f8d8051f5e6e1704f416988c73a6
Zuzana Sorokova <zuzanasorokova@macbook-pro-2.home>
2022-01-23 09:15:07 +0100

    uitwerking

--- functions-methods.js
+++ functions-methods.js
@@ -1,4 +1,4 @@
-// Je gaat functies schrijven die we kunnen hergebruiken om sommige emailadressen te checken. Nu zul je gaan merken hoe handig functies kunnen zijn!
+// Je gaat functies schrijven die gitwe kunnen hergebruiken om sommige emailadressen te checken. Nu zul je gaan merken hoe handig functies kunnen zijn!
 // Je zult hier methoden van het String Object voor nodig hebben, dus pak de paragraaf op EdHub over het String Object er even bij.
 
 
